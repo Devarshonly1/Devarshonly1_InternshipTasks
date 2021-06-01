5c882aa1a2cde61f48230545394a09eee481f41b
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-06-01 22:13:08 +0530

    Update 9.py

--- Day 3/9.py
+++ Day 3/9.py
@@ -4,6 +4,6 @@ if n1<100:
     if n1%2==0:
         print("the number is even :")
     else:
-        print("the number is odd .")
+        print("the number is odd :")
 else:
-    print("the number is not less than 100 .")
+    print("the number is not less than 100 :")
