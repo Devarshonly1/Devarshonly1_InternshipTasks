8819c35f060b7096148ed0279bb80411d97ab4ce
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-06-03 17:41:53 +0530

    Update 15.py

--- Day 3/15.py
+++ Day 3/15.py
@@ -1,11 +1,9 @@
-'''
+"""
 n1 =int(input("Enter the starting number : "))
 n2 =int(input("Enter the ending  number : "))
-
-
 for i in reversed(range(n2,n1+1,3)):
     print(i)
-    '''
+   """
 print("strating :")
 n1=int(input())
 print("ending :")
