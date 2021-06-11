d4b24ed428f34f0ede26facf0383ccb1a691a14b
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-06-11 13:13:47 +0530

    Add files via upload

---
+++ Day 4/Keywords.py
@@ -0,0 +1,5 @@
+def myfun(**arg):
+    for i,j in arg.items():
+        print(i,j)
+
+myfun(Name='Devarsh_only1', Branch='B.E.Computer Engineering') 
---
+++ Day 4/Membership.py
@@ -0,0 +1,11 @@
+v = 10
+b = 7
+list = [7,8,9,10,11]
+
+print(v in list)
+
+print(b in list)
+
+print(b not in list)
+
+print(v  not in list)
