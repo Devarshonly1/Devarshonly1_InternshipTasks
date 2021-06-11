302e2152f2a32af300c941b277be487d4cb3ce63
Devarshonly1 <84829980+Devarshonly1@users.noreply.github.com>
2021-06-11 12:36:59 +0530

    Add files via upload

---
+++ Day 4/comparision.py
@@ -0,0 +1,13 @@
+b = 30
+s = 24
+print('b > s is', b > s)
+
+print('b > s is', b > s)
+
+print('b < s is', b < s)
+
+print('b != s is', b != s)
+
+print('b >= s is', b >= s)
+
+print('b <= s is', b <= s)
